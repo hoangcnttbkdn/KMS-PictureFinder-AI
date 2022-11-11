66cb6c678bdcf470323f518217471e43f46c9cf2
huytuong010101 <huytuong010101@gmail.com>
2022-11-11 13:28:14 +0700

    fix/add numface and bbox in response _ refactor

--- src/app.py
+++ src/app.py
@@ -14,6 +14,4 @@ async def create_upload_files(list_images: List[UploadFile], target_image: Uploa
     index2file_name = [file.filename for file in list_images]
     results = face_findor(BGR_target_image, BGR_list_images)
     response = {index2file_name[int(key)]: results[key] for key in results}
-    print("aaaaa")
-    print(response)
     return response
