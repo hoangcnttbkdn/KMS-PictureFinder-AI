85fd7e2565ae2f8fa4adf4580eafa9c318374717
huytuong010101 <huytuong010101@gmail.com>
2022-12-10 11:41:51 +0700

    basic clothes findor using histogram

--- src/app.py
+++ src/app.py
@@ -3,9 +3,11 @@ from fastapi import FastAPI, File, UploadFile
 from src import utils
 
 from src.modules import FaceFindor
+from src.modules import ClothesCompare
 
 app = FastAPI()
 face_findor = FaceFindor()
+clothes_findor = ClothesCompare()
 
 @app.post("/face-findor")
 async def create_upload_files(list_images: List[UploadFile], target_image: UploadFile=File()):
@@ -14,4 +16,15 @@ async def create_upload_files(list_images: List[UploadFile], target_image: Uploa
     index2file_name = [file.filename for file in list_images]
     results = face_findor(BGR_target_image, BGR_list_images)
     response = {index2file_name[int(key)]: results[key] for key in results}
+    return response
+
+
+@app.post("/clothes-findor")
+async def clothes_findor_endpoint(list_images: List[UploadFile], target_image: UploadFile=File()):
+    BGR_target_image = await utils.file2opencv(target_image)
+    BGR_list_images = [await utils.file2opencv(file) for file in list_images]
+    index2file_name = [file.filename for file in list_images]
+    results = clothes_findor(BGR_target_image, BGR_list_images)
+    print(results)
+    response = {index2file_name[int(key)]: results[key] for key in range(len(results))}
     return response
